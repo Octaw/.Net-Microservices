d7d60b9a48df75b33acfda0151804293b08d1c9a
Octaw <j3zing@gmail.com>
2023-02-04 23:44:30 +0100

    init
    init commit

---
+++ Mango.Services.ProductAPI/Models/Dto/Product.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System.ComponentModel.DataAnnotations;
+namespace Mango.Services.ProductAPI.Models.Dto
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Range(1, 1000)]
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string CategoryName { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ Mango.Services.ProductAPI/Program.cs
@@ -0,0 +1,49 @@
+using AutoMapper;
+using Mango.Services.ProductAPI.DbContexts;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Repository;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer("Server = (localdb)\\localhost; Database = MangoProductAPI; Trusted_Connection = True; MultipleActiveResultSets = true"));
+            IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+            builder.Services.AddSingleton(mapper);
+            builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
