c3c766a6fdfc60d8007f2973da82478b20a21d88
Octaw <j3zing@gmail.com>
2023-02-17 17:54:31 +0100

    add a checkout functionality
    add a checkout functionality

--- Mango.Web/Controllers/CartController.cs
+++ Mango.Web/Controllers/CartController.cs
@@ -70,6 +70,28 @@ namespace Mango.Web.Controllers
             return View(await LoadCartDtoBasedOnLoggedInUser());
         }
 
+        [HttpPost]
+        public async Task<IActionResult> Checkout(CartDto cartDto)
+        {
+
+            try
+            {
+                var accesToken = await HttpContext.GetTokenAsync("access_token");
+                var response = await _cartService.Checkout<ResponseDto>(cartDto.CartHeader, accesToken);
+                return RedirectToAction(nameof(Confirmation));
+            }
+            catch(Exception ex)
+            {
+                return View(cartDto);
+            }
+        }
+
+        public async Task<IActionResult> Confirmation()
+        {
+
+            return View();
+        }
+
         private async Task<CartDto> LoadCartDtoBasedOnLoggedInUser()
         {
             var userId = User.Claims.Where(c => c.Type == "sub")?.FirstOrDefault()?.Value;
